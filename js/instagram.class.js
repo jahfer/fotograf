450d5c410dfc785f71a72e451d0bb0c872e56fbb
Jahfer Husain <jahferh@gmail.com>
2013-04-26 00:16:05 -0400

    Switched tabs to spaces

--- js/instagram.class.js
+++ js/instagram.class.js
@@ -1,39 +1,39 @@
 var INSTAGRAM = (function() {
-	var _parseToken = /^#access_token\=(.*)$/;
-	var _accessToken;
+    var _parseToken = /^#access_token\=(.*)$/;
+    var _accessToken;
 
-	function getTokenFromHash() {
-		var hash = window.location.hash;
-		if (hash) {
-			var token = _parseToken.exec(hash);
-			_accessToken = token[1];
-		}
-	}
+    function getTokenFromHash() {
+        var hash = window.location.hash;
+        if (hash) {
+            var token = _parseToken.exec(hash);
+            _accessToken = token[1];
+        }
+    }
 
-	function _api(uri) {
-		return function(callback) {
-			reqwest({
-				url: "https://api.instagram.com/v1/"+uri+"/?access_token=" + _accessToken,
-				type: 'jsonp',
-				success: callback
-			});
-		};
-	}
+    function _api(uri) {
+        return function(callback) {
+            reqwest({
+                url: 'https://api.instagram.com/v1/' + uri + '/?access_token=' + _accessToken,
+                type: 'jsonp',
+                success: callback
+            });
+        };
+    }
 
-	var _userInfo = _api("users/self");
-	var getLiked = _api("users/self/media/liked");
+    var _userInfo = _api('users/self');
+    var getLiked  = _api('users/self/media/liked');
 
-	var getUserPosts = function(callback) {
-		_userInfo(function(result) {
-			var userId = result.data.id;
-			var userFeed = _api("users/" + userId + "/media/recent");
-			userFeed(callback);
-		});
-	};
+    var getUserPosts = function(callback) {
+        _userInfo(function(result) {
+            var userId = result.data.id;
+            var userFeed = _api('users/' + userId + '/media/recent');
+            userFeed(callback);
+        });
+    };
 
-	return {
-		getTokenFromHash: getTokenFromHash,
-		getUserPosts: getUserPosts,
-		getLiked: getLiked
-	};
+    return {
+        getTokenFromHash: getTokenFromHash,
+        getUserPosts: getUserPosts,
+        getLiked: getLiked
+    };
 })();
